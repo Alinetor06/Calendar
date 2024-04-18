e8c67ea3b3eb2980aa9da59b13b298060982c11b
Alinetor06 <alessandro.palazzolo06@gmail.com>
2024-04-18 16:57:57 +0200

    change and init frontend

---
+++ example-app/tests/Feature/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_task_list_can_be_retrieved(): void
+    {
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['view-tasks']
+        );
+
+        $response = $this->get('/api/task');
+
+        $response->assertOk();
+    }
+
+
+}
